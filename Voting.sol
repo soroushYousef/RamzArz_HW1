eb2b2789ac90a5476ab04b20bfc5f83fd3753b72
Mohammadhossein Arsalan <61599235+ARSERLIN@users.noreply.github.com>
2022-11-20 15:02:46 +0330

    Final version + test

--- Voting.sol
+++ Voting.sol
@@ -46,6 +46,10 @@ contract Elections {
             msg.sender == Abbas,
             "you are not Abbas"
         );
+        require(
+            !time_check(),
+            "time is over"
+        );
         //miad tedad va hamintoor in ke sab nami tekrari nabashe ro check mikone
         bool is_registeration_valid = false;
         for(uint8 i = 0 ;i < _voters.length;i++){
@@ -87,8 +91,8 @@ contract Elections {
             target.is_registered,
             "target is not registered"
         );
-        target.available_votes += voter.available_votes;
-        voter.available_votes = 0;
+        voters[_target_voter].available_votes += voter.available_votes;
+        voters[msg.sender].available_votes = 0;
     }
     //baray ray dadan
     function vote (string memory _elected) public {
@@ -104,14 +108,14 @@ contract Elections {
             check kardan in ke sabt nam shode bashe 
         */
         require(
-            !voter.is_registered,
+            voter.is_registered,
             "You are not registerd" 
         );
         /*
             check kardan in ke zaman doroste 
         */
         require(
-            time_check(),
+            !time_check(),
             "time is over"
         );
         
@@ -131,7 +135,29 @@ contract Elections {
         );
         date_of_end = _date_of_end;
     }
+    function live_result() public view returns(uint){
+        uint total_votes = 0;
+        for(uint i =0 ; i < candidates.length;i++){
+             total_votes += votes[candidates[i]];
+        }
+        return total_votes;
+    }
 
+    function registered() public view returns(uint){
+        
+        return number_of_registered;
+    }
+
+    function showtime() public view returns(uint){
+        
+        return block.timestamp;
+    }
+     function end_of_voting() public{
+        require(
+            msg.sender == Abbas, "You are not Abbas"
+        );
+        date_of_end = block.timestamp;
+    }
     //elam natije
     function result () public view returns(string memory){
         //barresi etmam voting
@@ -146,15 +172,15 @@ contract Elections {
         for(uint i =0 ; i < candidates.length;i++){
              if(votes[candidates[i]] > max_votes){
                  max_votes = votes[candidates[i]];
-                 unique_winner = false;
+                 unique_winner = true;
                  winner = i;
              }else if(votes[candidates[i]] == max_votes){
-                 unique_winner = true;
+                 unique_winner = false;
              }
              total_votes += votes[candidates[i]];
         }
         require(
-            (total_votes / number_of_registered)*100 > 50, 
+            (total_votes*100 / number_of_registered) > 50, 
             "This election is cancelled due to lack of votes recieved."
         );
         if(unique_winner){
